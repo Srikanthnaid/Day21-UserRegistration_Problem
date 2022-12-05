5676c95275defdf63bfe511e6e5995f4101c248e
Srikanthnaid <ksru044gmail.com>
2022-12-06 00:03:15 +0530

    Password havinh at least one special character

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -7,8 +7,8 @@ public class UserRegistration {
 
 	public static void passwordCheck(String Password) {// taken a Parameter
 
-		// UC7 ->password Rule 3 password having at least one numeric value
-		Pattern p = Pattern.compile("^[A-Za-z0-9@#$!_-]{8,}$");
+		// UC8 ->password Rule 3 password having at least one special character
+		Pattern p = Pattern.compile("^[A-Za-z0-9@#$_!&^-]{8,}$");
 		Matcher m = p.matcher(Password);
 
 		if (m.matches()) {// true
