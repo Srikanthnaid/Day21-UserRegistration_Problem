138a75105fa32690be0904a650a609eca46902af
Srikanthnaid <ksru044gmail.com>
2022-12-06 00:54:30 +0530

    Junit test using to test the user enter

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -1,26 +1,48 @@
 package UserRegistration;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public static void validEmailCheck(String emails) {// taken a Parameter
-
-		// UC9 - >Valid E-Mails
-		Pattern p = Pattern.compile("^[a-zA-Z0-9]+[@][a-zA-Z]+[.][a-z]{2,3}$");
-		Matcher m = p.matcher(emails);
-
-		if (m.matches()) {// true
-			System.out.println("\n\t Email Valid");
-		} else { // false
-			System.out.println("! Not-Valid Password  \nPlease Enter proper one");
-		}
-
+	// UC10 - > J Unit Test
+	// uc1
+	public boolean firstNameCheck(String FirstName) {// taken a Parameter
+		return Pattern.matches("^[A-Z]{1}[a-z]{2,}$", FirstName);
 	}
 
-	public static void main(String[] args) {
-		System.out.println("-----------Welcome to UserRegistration------------");
-		validEmailCheck("srikanth019@gmail.com");// call the method
+	// uc2
+	public boolean lasNameCheck(String LastName) {
+		return Pattern.matches("^[A-Z]{1}[a-z]{2,}$", LastName);
 	}
+
+	// uc3
+	public boolean emailCheck(String Email) {
+		return Pattern.matches("^[a-zA-Z]+[.][A-Za-z]+@[a-zA-Z]+[.]+[a-zA-Z]{2}+[.][A-Za-z]{2}+$", Email);
+	}
+
+	// uc4
+	public boolean validMbnumber(String MbNumber) {
+		return Pattern.matches("^[1-9]{2}\\s[6-9]{1}[0-9]{9}$", MbNumber);
+	}
+
+	// uc5
+	public boolean passwordRule1Check(String password) {
+		return Pattern.matches("^[A-Za-z0-9@#$_-]{8,}", password);
+	}
+
+	// UC6
+	public boolean passwordrule2Check(String Password) {
+		return Pattern.matches("^[A-Z]{1}[A-Za-z0-9@._-]{7,}$", Password);
+	}
+
+	// uc7
+	public boolean passwordRule3Check(String Password) {
+		return Pattern.matches("^[A-Za-z0-9]{8,}$", Password);
+	}
+
+	// uc8
+	public boolean passwordRule4Check(String password) {
+		return Pattern.matches("^[A-Za-z0-9!/$%#_=@]{8,}$", password);
+	}
+
 }
---
+++ src/test/java/UserRegistration/UserRegistrationTest.java
@@ -0,0 +1,106 @@
+package UserRegistration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserRegistrationTest {
+
+	UserRegistration userRegistration = new UserRegistration();
+
+	@Test
+	public void givenFirstName_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.firstNameCheck("Srikanth");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenFirstName_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.firstNameCheck("surabhi");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenLastName_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.lasNameCheck("Naidu");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenLastName_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.lasNameCheck("naidu");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenEmail_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.emailCheck("abc.xyz@bl.co.in");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenEmail_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.emailCheck("srikanth@gmail.com");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenMobileNo_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.validMbnumber("91 9640604452");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenMobileNo_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.validMbnumber("919640604452");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenPasswordRule1_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.passwordRule1Check("Srikanth9640@");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenPasswordRule1_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.passwordRule1Check("srikanth)");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenPasswordRule2_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.passwordrule2Check("Sri@ka96");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenPasswordRule2_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.passwordrule2Check("srika.9640@");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenPasswordRule3_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.passwordRule3Check("Srikanth640");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenPasswordRule3_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.passwordRule3Check("Sri.@96");
+		assertFalse(result);
+	}
+
+	@Test
+	public void givenPasswordRule4_WhenValid_ShouldReturnsTrue() {
+		boolean result = userRegistration.passwordRule4Check("Srikan@9640");
+		assertTrue(result);
+	}
+
+	@Test
+	public void givenPasswordRule4_WhenInValid_ShouldReturnsFalse() {
+		boolean result = userRegistration.passwordRule4Check("srik015gmail.com");
+		assertFalse(result);
+	}
+}
