ca6ca44c3f35959e30e95dd612dde61b5bf7e2d2
Srikanthnaid <ksru044gmail.com>
2022-12-05 23:41:34 +0530

    Vallid E-mail

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -5,22 +5,22 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public static void emailCheck(String Email) {// taken a Parameter
+	public static void mobileNumberCheck(String Mbnumber) {// taken a Parameter
 
 		// UC3 ->Valid E-mail
-		Pattern p = Pattern.compile("^[a-zA-Z]+[.][A-Za-z]+@[a-zA-Z]+[.]+[a-zA-Z]{2}+[.][A-Za-z]{2}+$");
-		Matcher m = p.matcher(Email);
+		Pattern p = Pattern.compile("^[1-9]{2}\\s[6-9]{1}[0-9]{9}$");
+		Matcher m = p.matcher(Mbnumber);
 
 		if (m.matches()) {// true
-			System.out.println("\n\tLastName Valid");
+			System.out.println("\n\tMobile number Valid");
 		} else { // false
-			System.out.println("! Not-Valid LastName  \nPlease Enter proper one");
+			System.out.println("! Not-Valid mobile number  \nPlease Enter proper one");
 		}
 
 	}
 
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to UserRegistration------------");
-		emailCheck("abc.xyz@bl.co.in");// call the method
+		mobileNumberCheck("91 9640604452");// call the method
 	}
 }
