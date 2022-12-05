c683866571391230bba2c2d40c28930bae6aab7b
Srikanthnaid <ksru044gmail.com>
2022-12-05 23:15:03 +0530

    Welcome to UserRegistration

---
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -0,0 +1,12 @@
+package UserRegistration;
+
+public class UserRegistration {
+	
+	public static void nameCheck(String FirstName) {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("-----------Welcome to UserRegistration------------");
+	}
+}
