46b0710d745bad5cb89cb0e85f90294b07acc602
Srikanthnaid <ksru044gmail.com>
2022-12-05 23:24:00 +0530

    Enter a first name that must having start with cap letter

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -1,12 +1,25 @@
 package UserRegistration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	
 	public static void nameCheck(String FirstName) {
+		
+		Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		Matcher m = p.matcher(FirstName);
+		
+		if(m.matches()) {
+			System.out.println("\n\tFirstname Valid");
+		}else {
+			System.out.println("! Not-Valid FirstName  \nPlease Enter proper one");
+		}
 		
 	}
 
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to UserRegistration------------");
+		nameCheck("Srikanth");
 	}
 }
