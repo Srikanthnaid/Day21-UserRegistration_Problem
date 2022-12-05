199fbbe6aabd785fd6eaf68b2b1478551258db51
Srikanthnaid <ksru044gmail.com>
2022-12-05 23:36:16 +0530

    Valid Email

--- src/main/java/UserRegistration/UserRegistration.java
+++ src/main/java/UserRegistration/UserRegistration.java
@@ -4,22 +4,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	
-	public static void nameCheck(String LastName) {
-		
-		Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
-		Matcher m = p.matcher(LastName);
-		
-		if(m.matches()) {//true		
+
+	public static void emailCheck(String Email) {// taken a Parameter
+
+		// UC3 ->Valid E-mail
+		Pattern p = Pattern.compile("^[a-zA-Z]+[.][A-Za-z]+@[a-zA-Z]+[.]+[a-zA-Z]{2}+[.][A-Za-z]{2}+$");
+		Matcher m = p.matcher(Email);
+
+		if (m.matches()) {// true
 			System.out.println("\n\tLastName Valid");
-		}else {				//false
+		} else { // false
 			System.out.println("! Not-Valid LastName  \nPlease Enter proper one");
 		}
-		
+
 	}
 
 	public static void main(String[] args) {
 		System.out.println("-----------Welcome to UserRegistration------------");
-		nameCheck("Naidu");
+		emailCheck("abc.xyz@bl.co.in");// call the method
 	}
 }
